6bd785c8785387da22bccff2075b73dc9d7be531
Yong Jun Cha <perfect_yon@naver.com>
2024-02-18 21:57:02 +0900

    feat: add splash

---
+++ CommerceApp/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  CommerceApp
+//
+//  Created by Yong Jun Cha on 2/18/24.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
