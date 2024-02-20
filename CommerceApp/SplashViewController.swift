1d2d6804ce93bc0038727fe2b9f33e8d58369c45
Yong Jun Cha <perfect_yon@naver.com>
2024-02-20 16:59:21 +0900

    feat: add lottie library, modify splashView

--- CommerceApp/SplashViewController.swift
+++ CommerceApp/SplashViewController.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashViewController: UIViewController {
-
+    @IBOutlet weak var LottieAnimationView: LottieAnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        LottieAnimationView.play()
+    }
 
 }
